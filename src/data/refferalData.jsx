0e067adc16e8dd59aa23897efa0390b2393c41ea
Omkar-Jawalkar <omkar@goreeva.com>
2024-01-20 18:25:41 +0530

    Data added

--- src/data/refferalData.jsx
+++ src/data/refferalData.jsx
@@ -12,4 +12,25 @@ export const data = [
         closing: "I look forward to hearing back from you soon.",
         signature: "Best regards, #myName#",
     },
+
+    // Hello Annwesa,
+    // Hope you are doing well.
+    // I am looking for work opportunities and have found an exciting role
+    //  Software Engineer (Fresher Hiring) at your organization.
+    // I would love to have a conversation with you and would also be very
+    // grateful if you could refer me.
+    //   I can ASSURE you it won't be a waste of time.
+    {
+        greeting: "Hello #employee_name#",
+        body: "Hope you are doing well.",
+        opening:
+            "I am looking for work opportunities and have found an exciting role #role# (Fresher Hiring) at your organization.",
+        reason: "",
+        request:
+            "I would love to have a conversation with you and would also be very grateful if you could refer me.",
+        resume: "",
+        gratitude: "I can ASSURE you it won't be a waste of time.",
+        closing: "",
+        signature: "",
+    },
 ];
